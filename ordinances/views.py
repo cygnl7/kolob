a2c5181eb3167b9a49a3d8c4801e23a85275dc16
David Brown <david@davtar.org>
2013-06-02 16:19:32 -0600

    Make all-ward goals work

--- ordinances/views.py
+++ ordinances/views.py
@@ -1,17 +1,28 @@
 from django.http import HttpResponse
+from django.db.models import Sum
 from django.template import Context, loader
 
-from ordinances.models import Ancestor
+from ordinances.models import Ancestor, Ward
+
+ALL_WARDS = 'Lehi Utah South Celestial Stake'
 
 def index(request):
-    user_ward = ''
-    ward_member_goal = 0
-    ward_ordinance_goal = 0
+    user_ward = ALL_WARDS
+    member_goal = 0
+    ordinance_goal = 0
     if hasattr(request.user, 'wardmember'):
         user_ward = request.user.wardmember.ward.name
-        ward_member_goal = request.user.wardmember.ward.member_goal
-        ward_ordinance_goal= request.user.wardmember.ward.ordinance_goal
-    all_ancestors_list = Ancestor.objects.filter(ward__name = user_ward).order_by('surname', 'given_name')
+        member_goal = request.user.wardmember.ward.member_goal
+        ordinance_goal= request.user.wardmember.ward.ordinance_goal
+
+    if (user_ward == ALL_WARDS):
+        all_ancestors_list = Ancestor.objects.all().order_by('surname', 'given_name')
+        # We have to calculate the goals as an aggregate of all wards
+        member_goal = Ward.objects.aggregate(Sum('member_goal'))['member_goal__sum']
+        ordinance_goal = Ward.objects.aggregate(Sum('ordinance_goal'))['ordinance_goal__sum']
+    else:
+        all_ancestors_list = Ancestor.objects.filter(ward__name = user_ward).order_by('surname', 'given_name')
+        
     baptism_count = all_ancestors_list.filter(baptism_date__year = 2013).count()
     confirmation_count = all_ancestors_list.filter(confirmation_date__year = 2013).count()
     initiatory_count = all_ancestors_list.filter(initiatory_date__year = 2013).count()
@@ -24,7 +35,7 @@ def index(request):
         'all_ancestors_list': all_ancestors_list,
         'ordinance_count': ordinance_count,
         'ward_name': user_ward,
-        'ward_member_goal': ward_member_goal,
-        'ward_ordinance_goal': ward_ordinance_goal
+        'member_goal': member_goal,
+        'ordinance_goal': ordinance_goal
     })
     return HttpResponse(template.render(context))
