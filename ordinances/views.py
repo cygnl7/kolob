368a6889b16bd54e828e655e170cfa103a2bd881
David Brown <david@davtar.org>
2013-06-02 16:50:59 -0600

    Accept ward name in request params

--- ordinances/views.py
+++ ordinances/views.py
@@ -7,22 +7,36 @@ from ordinances.models import Ancestor, Ward
 ALL_WARDS = 'Lehi Utah South Celestial Stake'
 
 def index(request):
-    user_ward = ALL_WARDS
-    member_goal = 0
-    ordinance_goal = 0
-    if hasattr(request.user, 'wardmember'):
-        user_ward = request.user.wardmember.ward.name
-        member_goal = request.user.wardmember.ward.member_goal
-        ordinance_goal= request.user.wardmember.ward.ordinance_goal
+    # Default to all wards
+    user_ward_name = ALL_WARDS
+    if ('ward' in request.REQUEST):
+        # Use the request's ward if available
+        user_ward_name = request.REQUEST['ward']
+    elif hasattr(request.user, 'wardmember'):
+        # Default to the ward the user is in
+        user_ward_name = request.user.wardmember.ward.name
 
-    if (user_ward == ALL_WARDS):
-        all_ancestors_list = Ancestor.objects.all().order_by('surname', 'given_name')
+    # See if we have a valid ward name, ALL_WARDS is always valid
+    wardlist = [] # Save for later, too
+    if (user_ward_name != ALL_WARDS):
+        wardlist = Ward.objects.filter(name = user_ward_name)
+        if (wardlist.count() < 1):
+            user_ward_name = ALL_WARDS
+
+    # Now figure out goals and ancestor list
+    member_goal = 1 # If we somehow don't get a goal, avoid using 0 here so we don't divide by 0
+    ordinance_goal = 1 # Same thing as ^
+    if (user_ward_name == ALL_WARDS):
+        all_ancestors_list = Ancestor.objects.order_by('surname', 'given_name')
         # We have to calculate the goals as an aggregate of all wards
         member_goal = Ward.objects.aggregate(Sum('member_goal'))['member_goal__sum']
         ordinance_goal = Ward.objects.aggregate(Sum('ordinance_goal'))['ordinance_goal__sum']
     else:
-        all_ancestors_list = Ancestor.objects.filter(ward__name = user_ward).order_by('surname', 'given_name')
-        
+        all_ancestors_list = Ancestor.objects.filter(ward__name = user_ward_name).order_by('surname', 'given_name')
+        if (wardlist.count() > 0):
+            member_goal = wardlist[0].member_goal
+            ordinance_goal= wardlist[0].ordinance_goal
+
     baptism_count = all_ancestors_list.filter(baptism_date__year = 2013).count()
     confirmation_count = all_ancestors_list.filter(confirmation_date__year = 2013).count()
     initiatory_count = all_ancestors_list.filter(initiatory_date__year = 2013).count()
@@ -34,7 +48,7 @@ def index(request):
     context = Context({
         'all_ancestors_list': all_ancestors_list,
         'ordinance_count': ordinance_count,
-        'ward_name': user_ward,
+        'ward_name': user_ward_name,
         'member_goal': member_goal,
         'ordinance_goal': ordinance_goal
     })
